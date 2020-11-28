55d6e42e8e03ca708bfda56f5641f3747514e3a2
Admin <devenderaspldelhi@gmail.com>
2020-11-28 17:40:09 +0530

    28nov

---
+++ app/src/main/java/com/example/teamunited/Activity/Video_playing_Activity.java
@@ -0,0 +1,16 @@
+package com.example.teamunited.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teamunited.R;
+
+public class Video_playing_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_playing_);
+    }
+}
